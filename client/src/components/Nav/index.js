2bae77323b5fcab89f6913d535347d426bb7139a
Andrew Lin <andrewlin618@gmail.com>
2020-02-12 19:19:44 -0800

    ready for heroku

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav(){
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand" to='/'>Google Books</Link>
+                <Link className="navbar-brand" to='/'><i className="fas fa-book-open" /> React Bookshelf</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
