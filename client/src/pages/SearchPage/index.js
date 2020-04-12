030409a629821ec370b4dccaa88e574a92735e16
Andrew Lin <andrewlin618@gmail.com>
2020-04-12 02:45:28 -0500

    made url https

--- client/src/pages/SearchPage/index.js
+++ client/src/pages/SearchPage/index.js
@@ -176,9 +176,9 @@ class SearchPage extends React.Component {
                         categories={book.volumeInfo.categories} 
                         publisher={book.volumeInfo.publisher}
                         publishedDate={book.volumeInfo.publishedDate} 
-                        image={'http:' + book.volumeInfo.imageLinks.thumbnail.slice(4)} 
+                        image={'https' + book.volumeInfo.imageLinks.thumbnail.slice(4)} 
                         description={book.volumeInfo.description}  
-                        link={'http:' + book.volumeInfo.previewLink.slice(4)} 
+                        link={'https' + book.volumeInfo.previewLink.slice(4)} 
                         isCollapsed={this.state.isCollapsed}
                         target="_blank"
                         Buttonaaa={()=>(
